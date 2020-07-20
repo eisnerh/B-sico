d5ee522bc668c3f04dea704fa3ec2f8507be3fee
Eisner López Acevedo <elopeza@ucatolica.ac.cr>
2020-07-19 18:59:10 -0600

    Agregar archivos de proyecto.

---
+++ Basico/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Basico
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEjecuto = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtByte = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.lblRevisaBite = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnEjecuto
+            // 
+            this.btnEjecuto.Location = new System.Drawing.Point(12, 355);
+            this.btnEjecuto.Name = "btnEjecuto";
+            this.btnEjecuto.Size = new System.Drawing.Size(224, 52);
+            this.btnEjecuto.TabIndex = 0;
+            this.btnEjecuto.Text = "Ejecuto";
+            this.btnEjecuto.UseVisualStyleBackColor = true;
+            this.btnEjecuto.Click += new System.EventHandler(this.btnEjecuto_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(142, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Bienvenido";
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Location = new System.Drawing.Point(609, 355);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(179, 52);
+            this.btnCerrar.TabIndex = 2;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(145, 85);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(271, 22);
+            this.txtNombre.TabIndex = 3;
+            // 
+            // txtByte
+            // 
+            this.txtByte.Location = new System.Drawing.Point(145, 150);
+            this.txtByte.Name = "txtByte";
+            this.txtByte.Size = new System.Drawing.Size(271, 22);
+            this.txtByte.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(142, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Byte";
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(145, 178);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(271, 52);
+            this.btnCheck.TabIndex = 6;
+            this.btnCheck.Text = "Revisar si es Byte";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // lblRevisaBite
+            // 
+            this.lblRevisaBite.AutoSize = true;
+            this.lblRevisaBite.Location = new System.Drawing.Point(422, 153);
+            this.lblRevisaBite.Name = "lblRevisaBite";
+            this.lblRevisaBite.Size = new System.Drawing.Size(0, 17);
+            this.lblRevisaBite.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblRevisaBite);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtByte);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnEjecuto);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEjecuto;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCerrar;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtByte;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Label lblRevisaBite;
+    }
+}
+
---
+++ Basico/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Basico
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnEjecuto_Click(object sender, EventArgs e)
+        {
+            int numCaracteres = txtNombre.TextLength;
+
+            if (numCaracteres == 0)
+            {
+                MessageBox.Show("Debes escribir un nombre");
+            }
+            else
+            {
+                MessageBox.Show("Hola " + txtNombre.Text + "!!!");
+            }
+
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            
+
+            try
+            {
+                byte revisar = Convert.ToByte(txtByte.Text);
+
+                lblRevisaBite.Text = "Esta dentro del rango byte";
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Un Byte debe ser menor a 256");
+                
+                lblRevisaBite.Text = "No es byte";
+            }
+
+        }
+    }
+}
